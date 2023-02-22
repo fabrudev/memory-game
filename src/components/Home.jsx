530b0bb6e9a02c0c3604093f423f3bedd90580b2
FabruDJ <fabrizarateok@gmail.com>
2023-02-22 04:09:57 -0300

    fixes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import './HomeStyles.css'
 
 const Home = () => {
+  const navigate = useNavigate()
 
-    const navigate = useNavigate();
+  const goToGame = () => {
+    navigate('/game')
+  }
 
-    const goToGame = () => {
-        navigate("/game");
-    }
+  return (
+    <div className='home'>
+      <h1 className='home__title'>Memory Game!</h1>
+      <h2 className='home__subtitle'>by: Fabru-Dev</h2>
+      <button onClick={goToGame} className='home__button'>CLICK TO PLAY</button>
+    </div>
+  )
+}
 
-    return (
-        <div className='home'>
-            <h1 className='home__title'>Memory Game!</h1>
-            <h2 className='home__subtitle'>by: Fabru-Dev</h2>
-            <button onClick={goToGame} className='home__button'>CLICK TO PLAY</button>
-        </div>
-    );
-};
-
-export default Home;
+export default Home
